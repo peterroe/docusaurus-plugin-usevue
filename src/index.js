36bd29c107dad8cea44cd49109b204630e581272
Peter roe <peterroe@163.com>
2021-10-15 22:57:42 +0800

    quit webpack

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@
  * @Author: 林舒恒
  * @Date: 2021-10-15 21:30:12
  * @LastEditors: 林舒恒
- * @LastEditTime: 2021-10-15 22:23:36
+ * @LastEditTime: 2021-10-15 22:57:02
  */
-const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = (content,options) => {
     return {
