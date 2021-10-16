4f838cd6b5577b005fb3f30944440b9205eb2326
Peter roe <peterroe@163.com>
2021-10-16 13:19:52 +0800

    :fire: webpack

--- index.js
+++ index.js
@@ -3,28 +3,13 @@
  * @Author: 林舒恒
  * @Date: 2021-10-15 21:30:12
  * @LastEditors: 林舒恒
- * @LastEditTime: 2021-10-16 12:57:10
+ * @LastEditTime: 2021-10-16 13:04:04
  */
 const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = (content,options) => {
     return {
         name: 'docusaurus-plugin-usevue',
-        async contentLoaded({content, actions}) {
-            const {setGlobalData} = actions
-            let reactive = {}
-            let count = 0
-            Object.defineProperty(reactive,'uuid',{
-                get() {
-                    count ++
-                    return count
-                },
-                set(newv) {
-                    return newv
-                }
-            })
-            setGlobalData(reactive);
-        },
         configureWebpack(config, isServer, utils) {
             return {
                 module: {
