cfaf89aa45eeab9a0e97075ab9e9c3fc8d52a376
Peter roe <peterroe@163.com>
2021-10-24 19:58:57 +0800

    :fire: 去除操作文件记录的注释

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*
- * @Descripttion: 
- * @Author: 林舒恒
- * @Date: 2021-10-15 21:30:12
- * @LastEditors: 林舒恒
- * @LastEditTime: 2021-10-24 19:56:59
- */
 const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = (content,options) => {
